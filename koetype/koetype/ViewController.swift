70d04cd7196e12b060cad3730a679e090636a226
曽和修平 <sowashuhei@gmail.com>
2015-05-30 18:47:12 +0900

    init project

---
+++ koetype/koetype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  koetype
+//
+//  Created by 曽和修平 on 2015/05/30.
+//  Copyright (c) 2015年 曽和修平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
